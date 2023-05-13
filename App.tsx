7dc9d969b1a46a4b9fac6dd0a18d040fc9dd1318
fermar8 <ferran.ms8@gmail.com>
2023-05-13 15:17:31 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * React Native App
+ * https://github.com/fermar8/coach-app-client
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Try editing me! 🎉</Text>
+    </View>
+  );
+}
+
+export default App;
---
+++ src/stores/reducers/authenticationReducer.ts
@@ -0,0 +1,48 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+export interface Authentication {
+  isLoggedIn: boolean;
+  isLoading: boolean;
+  user: User;
+}
+
+interface User {
+  teams: string[];
+  leagues: string[];
+  name: string;
+  email: string;
+  _id: string;
+  createdAt: string;
+}
+
+const initialState: Authentication = {
+  isLoggedIn: false,
+  isLoading: true,
+  user: {
+    teams: [],
+    leagues: [],
+    name: '',
+    email: '',
+    _id: '',
+    createdAt: '',
+  },
+};
+
+export const authenticationSlice = createSlice({
+  name: 'Authentication',
+  initialState,
+  reducers: {
+    loginReducer: (state, action: PayloadAction<User>) => {
+      state.isLoggedIn = true;
+      state.isLoading = false;
+      state.user = action.payload;
+    },
+    logoutReducer: state => {
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const {loginReducer, logoutReducer} = authenticationSlice.actions;
+
+export default authenticationSlice.reducer;
